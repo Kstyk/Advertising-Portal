56e282510d2a5d06dd4fd0fa37f588081a51dba4
Seweryn Drag <dragseweryn@zohomail.eu>
2023-06-24 12:12:30 +0200

    Stowrzenie potrzebnych DTO do obslugi żądań i odpowiednich responsow

---
+++ ZleceniaAPI/Models/AddOfferDto.cs
@@ -0,0 +1,11 @@
+﻿using ZleceniaAPI.Entities;
+
+namespace ZleceniaAPI.Models
+{
+    public class AddOfferDto
+    {
+        public string Content { get; set; }
+        public Decimal Price { get; set; }
+        public string PriceFor { get; set; } // całość, za godzinę, za sztukę
+    }
+}
---
+++ ZleceniaAPI/Models/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ZleceniaAPI.Models
+{
+    public class AddressDto
+    {
+        public int Id { get; set; }
+        public string Voivodeship { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string? Street { get; set; }
+        public string? BuildingNumber { get; set; }
+    }
+}
