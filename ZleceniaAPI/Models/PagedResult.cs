4f1321589315f2d37a5aa74f3e2fe368f626f1cb
Seweryn Drag <dragseweryn@zohomail.eu>
2023-07-11 19:44:21 +0200

    Dodanie opcjonalnej listy kategorii usera

--- ZleceniaAPI/Models/PagedResult.cs
+++ ZleceniaAPI/Models/PagedResult.cs
@@ -3,6 +3,7 @@
     public class PagedResult<T>
     {
         public List<T> Items { get; set; }
+        public List<UserCategoryDto>? Categories { get; set; }
         public int TotalItemsCount { get; set; }
         public int TotalPages { get; set; }
         public int PageNumber { get; set; }
@@ -12,6 +13,17 @@
         public PagedResult(List<T> items, int totalCount, int pageSize, int pageNumber)
         {
             Items = items;
+            TotalItemsCount = totalCount;
+            PageNumber = pageNumber;
+            ItemsFrom = pageSize * (pageNumber - 1) + 1;
+            ItemsTo = ItemsFrom + pageSize - 1;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        }
+
+        public PagedResult(List<T> items, List<UserCategoryDto>? categories, int totalCount, int pageSize, int pageNumber)
+        {
+            Items = items;
+            Categories = categories;
             TotalItemsCount = totalCount;
             PageNumber = pageNumber;
             ItemsFrom = pageSize * (pageNumber - 1) + 1;
